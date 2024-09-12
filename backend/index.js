7030e2ea99b68ce304ccd2bfd09e4a22c13cc80e
saaisyvendra <saaisyvendras@gmail.com>
2024-09-12 14:33:50 +0530

    Added files

--- backend/index.js
+++ backend/index.js
@@ -4,17 +4,18 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 
 import userRoute from "./routes/UserRoute.js";
-// import customerRoute from "./routes/CustomerRoute.js";
-// import accountRoute from "./routes/AccountRoute.js";
-// import loanRoute from "./routes/LoanRoute.js";
-// import reportRoute from "./routes/ReportRoute.js";
+import accountRoute from "./routes/AccountRoute.js";
+import customerRoute from "./routes/CustomerRoute.js";
+import loanRoute from "./routes/LoanRoute.js";
+import fdRoute from "./routes/fdRoute.js";
+import reportRoute from "./routes/ReportRoute.js";
 import { authenticateJWT, authorizeRole } from "./middleware/auth.js";
 dotenv.config();
 
 const app = express();
 app.use(
     cors({
-        origin: "http://localhost:5173", // replace with your frontend URL
+        origin: "http://localhost:5173",
         credentials: true,
     })
 );
@@ -22,10 +23,11 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/user", userRoute);
-// app.use("/customer", authenticateJWT, customerRoute);
-// app.use("/account", authenticateJWT, accountRoute);
-// app.use("/loan", authenticateJWT, loanRoute);
-// app.use("/report", authenticateJWT, reportRoute);
+app.use("/account", authenticateJWT, accountRoute);
+app.use("/customer", authenticateJWT, customerRoute);
+app.use("/loan", authenticateJWT, loanRoute);
+app.use("/fd", authenticateJWT, fdRoute);
+app.use("/report", authenticateJWT, reportRoute);
 
 // Start the server
 const PORT = process.env.PORT;
