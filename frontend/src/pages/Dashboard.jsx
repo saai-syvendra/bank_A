70797c5e075f41981682ae4f39ba88c01203a02d
saaisyvendra <saaisyvendras@gmail.com>
2024-09-12 14:31:24 +0530

    dashboard security

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -3,17 +3,24 @@ import CustomerDashboard from "./CustomerDashboard";
 import EmployeeDashboard from "./EmployeeDashboard";
 import useTokenExpiration from "../auth/TokenExpiration";
 import AdminDashboard from "./AdminDashboard";
+import { useNavigate } from "react-router-dom";
 import { logout } from "../api/UserApi";
 
 const Dashboard = () => {
     useTokenExpiration();
+    const navigate = useNavigate();
     const role = localStorage.getItem("role");
+
     if (role === "customer") {
         return <CustomerDashboard />;
     } else if (["employee", "manager"].includes(role)) {
         return <EmployeeDashboard />;
     } else if (role === "admin") {
         return <AdminDashboard />;
+    } else {
+        logout();
+        navigate("/login");
+        return null;
     }
 };
 
