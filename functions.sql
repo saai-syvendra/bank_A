20bc65d4c2f69350d1ccc79ab2fb63faee0268e4
erandatheewijeratne <erandatheewijeratne@gmail.com>
2024-09-20 20:32:48 +0530

    Added bank transaction procedures, and functions.sql file

---
+++ functions.sql
@@ -0,0 +1,20 @@
+DELIMITER $$
+
+DROP FUNCTION IF EXISTS AccountExists;
+
+CREATE FUNCTION AccountExists(
+    p_account_id INT
+) RETURNS BOOLEAN
+DETERMINISTIC
+BEGIN
+    DECLARE v_exists BOOLEAN;
+
+    -- Check if the account exists
+    SELECT COUNT(*) > 0 INTO v_exists
+    FROM Customer_Account
+    WHERE account_id = p_account_id;
+
+    RETURN v_exists;
+END$$
+
+DELIMITER ;
