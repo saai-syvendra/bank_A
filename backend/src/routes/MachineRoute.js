4d3933f14d5efa30a0a601ffb067bba52e56da91
saaisyvendra <saaisyvendras@gmail.com>
2024-10-03 22:34:02 +0530

    JWT for cdm

--- backend/src/routes/MachineRoute.js
+++ backend/src/routes/MachineRoute.js
@@ -1,10 +1,16 @@
 import express from "express";
 import MachineController from "../controllers/MachineController.js";
+import { authenticateJWT, authorizeRole } from "../middleware/auth.js";
 
 const router = express.Router();
 
 router.get("/get-cdm-info", MachineController.getCDMinformation);
 
-router.post("/cdm-deposit", MachineController.makeCDMdeposit);
+router.post(
+  "/cdm-deposit",
+  authenticateJWT,
+  authorizeRole("machine"),
+  MachineController.makeCDMdeposit
+);
 
 export default router;
