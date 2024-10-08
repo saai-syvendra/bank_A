549ae2c4387e63e05610782419cc65eca98e3d40
erandatheewijeratne <erandatheewijeratne@gmail.com>
2024-10-08 12:21:40 +0530

    Changed a few Trasaction procedures

--- functions.sql
+++ functions.sql
@@ -1,10 +1,12 @@
-DELIMITER $$
-
 -- DROP FUNCTION IF EXISTS AccountExists;
+
+DELIMITER $$
 
 CREATE FUNCTION AccountExists(
     p_account_id INT
+
 ) RETURNS BOOLEAN
+
 DETERMINISTIC
 BEGIN
     DECLARE v_exists BOOLEAN;
@@ -12,7 +14,8 @@ BEGIN
     -- Check if the account exists
     SELECT COUNT(*) > 0 INTO v_exists
     FROM Customer_Account
-    WHERE account_id = p_account_id;
+    WHERE account_id = p_account_id
+    AND status = 'active';
 
     RETURN v_exists;
 END$$
